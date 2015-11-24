38853a87f05f97c17bbc917bea6238469078dcca
Jacqueline <jacqueline.kovarik@state.mn.us>
2015-11-24 15:54:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Initialize the map on the "map" div with a given center and zoom
+// Initialize the map 
 var map = L.map('map', {
-    center: [44.95, -93.09], // Centered on St. Paul (Lat/Long tool: http://itouchmap.com/latlong.html)
+    center: [44.95, -93.09], 
     zoom: 11 // Zoom level
 });
 
-/* Basemap: Thunderforest Spinal Map */
-/* Other Leaflet.js Basemaps: http://leaflet-extras.github.io/leaflet-providers/preview */
-/* To add the basemap to the map, use the .addTo(map) call */
 L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 }).addTo(map);
